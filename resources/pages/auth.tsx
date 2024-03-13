a273a5f99653ea7385a2d2d5e30618190546b819
Charles Lapierre <charles.lapierre@infinite-lab.com>
2024-03-13 19:43:40 +0100

    feat: add title login page

--- resources/pages/auth.tsx
+++ resources/pages/auth.tsx
@@ -2,7 +2,8 @@ import AuthDialog from '../components/auth_dialog'
 
 const AuthPage = () => {
   return (
-    <div className="w-full h-screen flex justify-center pt-48">
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-8 -mt-40">
+      <h1 className="text-6xl font-geist">Blindmedley</h1>
       <AuthDialog />
     </div>
   )

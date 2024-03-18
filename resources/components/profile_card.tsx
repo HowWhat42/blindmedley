2257972729a665bd6bb4b161e2fa854799b07480
Emyxam <maxime.olloix.63@gmail.com>
2024-03-18 20:21:05 +0100

    feat: profilecard (WIP)

--- resources/components/profile_card.tsx
+++ resources/components/profile_card.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const ProfileCard = () => {
-  return <div>ProfileCard</div>
+  return (
+    <div className="mt-32 flex h-screen content-center rounded-3xl border-4 border-solid border-sky-500 bg-red-500">
+      <div className="mx-auto flex content-center">
+        <div className="-mt-32 size-64 rounded-full border-4 border-solid border-sky-500 bg-purple-500"></div>
+      </div>
+    </div>
+  )
 }
 
 export default ProfileCard
--- resources/pages/profile.tsx
+++ resources/pages/profile.tsx
@@ -1,8 +1,10 @@
 import { Head, usePage } from '@inertiajs/react'
 
+import { Layout } from '#components/Layout/layout'
+
 import ProfileCard from '../components/profile_card'
 
-export default function Home() {
+export default function Profile() {
   const { user } = usePage<any>().props
 
   return (
@@ -14,4 +16,9 @@ export default function Home() {
       </div>
     </>
   )
+}
+
+Profile.layout = (page: any) => {
+  const { user } = page.props
+  return <Layout user={user}>{page}</Layout>
 }
